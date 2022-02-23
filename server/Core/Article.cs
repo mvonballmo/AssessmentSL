b78ebd6ac02bf7f60d0c4b0c5e13e852831372a5
Marco von Ballmoos <marco@earthli.com>
2022-02-23 13:23:13 +0100

    Reduce clutter in Article declaration

--- server/Core/Article.cs
+++ server/Core/Article.cs
@@ -2,15 +2,6 @@ using System.Data.Common;
 
 namespace Core;
 
-public class Article
-{
-    public int Id { get; set; }
-
-    public bool Featured { get; set; }
-
-    public string? Title { get; set; }
-
-    public string? ImageUrl { get; set; }
-
-    public string? NewsSite { get; set; }
-}
+public record Article(
+    int Id, bool Featured, string? Title, string? ImageUrl
+);

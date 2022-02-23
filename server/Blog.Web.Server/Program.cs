90c84da9d0e58b7bc990ae9d1178f2f07ef9f5f5
Marco von Ballmoos <marco@earthli.com>
2022-02-23 13:41:44 +0100

    Add CORS support

--- server/Blog.Web.Server/Program.cs
+++ server/Blog.Web.Server/Program.cs
@@ -4,6 +4,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(b => b
+        .WithOrigins("http://localhost:3000/")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+    );
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,6 +29,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
